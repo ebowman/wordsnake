9ad75ea7f2c1dec1670a7a89a1c8c449837fd875
Eric Bowman <ebowman@gilt.com>
2012-04-30 23:45:45 +0100

    First working

---
+++ Wordsnake.scala
@@ -0,0 +1,69 @@
+import collection.mutable.ListBuffer
+
+object Wordsnake extends App {
+
+  val allWords = args.toSet
+
+  var allSeeds = allWords.toList.combinations(2).map {
+    f: List[String] =>
+      val a = f.head
+      val b = f.tail.head
+      Result(Set(a, b), merge(a, b))
+  }.toSeq
+
+  var done = false
+  while (!done) {
+    allSeeds = allSeeds.flatMap(_.advance)
+    done = allSeeds.head.words == allWords
+  }
+  var shortSolutions = allSeeds.map(_.best)
+  allSeeds = null
+  var best = null: String
+  while (!shortSolutions.isEmpty) {
+    val intermediate = shortSolutions.take(10000).par.reduce((a, b) => if (a.length < b.length) a else b)
+    shortSolutions = shortSolutions.drop(10000)
+    if (best == null) {
+      best = intermediate
+    } else {
+      if (intermediate.length < best.length) {
+        best = intermediate
+        println(best + " (" + best.length + ")")
+      }
+    }
+  }
+  print(best)
+
+  case class Result(words: Set[String], solutions: Set[String]) {
+    def advance: Iterable[Result] = {
+      for {
+        word <- allWords if !words.contains(word)
+        b <- solutions
+      } yield copy(words + word, merge(word, b))
+    }
+    def best: String = {
+      solutions.reduce((a, b) => if (a.length < b.length) a else b)
+    }
+  }
+
+  def merge(a: String, b: String): Set[String] = {
+    if (a.length > b.length) {
+      merge(b, a)
+    } else {
+      val buffer = new ListBuffer[String]
+      buffer += a+b
+      buffer += b+a
+      if (b.contains(a)) {
+        buffer += b
+      }
+      for (i <- 1 until a.length) {
+        if (a.drop(a.length - i) == b.take(i)) {
+          buffer += a.take(a.length - i) + b
+        }
+        if (a.take(i) == b.drop(b.length - i)) {
+          buffer += b + a.drop(i)
+        }
+      }
+      buffer.toSet
+    }
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,22 @@
+
+name := "wordsnake"
+
+organization := "com.gilt"
+
+version := "0.0.1"
+
+scalaVersion := "2.9.1"
+
+resolvers ++= Seq(
+    "external" at "https://nexus.gilt.com/nexus/repositories/content/repositories/sonatype.releases")
+
+// libraryDependencies ++= Seq("com.typesafe.akka" % "akka-actor" % "2.0")
+
+scalacOptions ++= Seq("-deprecation", "-unchecked", "-optimize")
+
+mainClass in (Compile, packageBin) := Some("Fast") 
+
+//seq(com.twitter.sbt.GitProject.gitSettings: _*)
+
+//seq(com.twitter.sbt.PackageDist.newSettings: _*)
+
